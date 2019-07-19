4ced29a1ce8684a567d5a3cd4c34be8968c03f57
稲田 <@TSD-INADA>
2019-07-19 14:55:56 +0900

    DBUtils

--- src/com/abc/asms/goods/utils/DBUtils.java
+++ src/com/abc/asms/goods/utils/DBUtils.java
@@ -15,7 +15,7 @@ public class DBUtils {
 			throws NamingException, SQLException {
 		Context initContext = new InitialContext();
 		Context envContext = (Context) initContext.lookup("java:/comp/env");
-		DataSource ds = (DataSource) envContext.lookup("asms");
+		DataSource ds = (DataSource) envContext.lookup("project2");
 		return ds.getConnection();
 	}
 
