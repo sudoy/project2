3ac815a89a3fdc9e517464a2f67ab06175e5690b
藤ノ木 <@TSD-FUJINOKI>
2019-07-26 09:07:46 +0900

    修正

--- src/com/abc/asms/sales/S0025Servlet.java
+++ src/com/abc/asms/sales/S0025Servlet.java
@@ -105,7 +105,7 @@ public class S0025Servlet extends HttpServlet {
 
 			//権限チェック(権限が無い場合はダッシュボードへ遷移)
 			C0010Form checkauthority1 = (C0010Form) session.getAttribute("accounts");
-			if (!checkauthority1.getAuthority().equals("10") && !checkauthority1.getAuthority().equals("11")) {
+			if (!checkauthority1.getAuthority().equals("1") && !checkauthority1.getAuthority().equals("11")) {
 				error.add("不正なアクセスです。");
 				session.setAttribute("error", error);
 				resp.sendRedirect("C0020.html");
